498df46d2a1805b50da5ab7775e49d5a2e12cae5
Hwangjisoo <ghkdwltn8@gmail.com>
2021-11-05 14:52:41 +0900

    pprint modify

--- manual_data_insert/meta_read_write.py
+++ manual_data_insert/meta_read_write.py
@@ -12,7 +12,6 @@ function names
 - update* : modify or append infomation of some metadata
 - read* : read data from mongodb
 """
-from pprint import pp
 import sys,os
 # add a directory path of this file in python's path
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
