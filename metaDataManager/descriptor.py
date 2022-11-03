991690a6c6bd5210ca52e89dc7e3438121834e71
hwangjisoo <ghkdwltn8@gmail.com>
2022-11-03 12:00:23 +0900

    descriptor update

--- metaDataManager/descriptor.py
+++ metaDataManager/descriptor.py
@@ -5,21 +5,19 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(
 
 from KETIToolMetaManager.metaDataManager import wizMongoDbApi as wiz
 
-class WriteData():
-    def __init__(self, metasave_info, meta_data):
-        self.db = metasave_info["databaseName"]
-        self.mode = metasave_info["mode"]
-        self.meta_data = meta_data
-        self.ms_list = metasave_info["measurementsName"]
+def write_data(metasave_info, meta_data):
+    db_name = metasave_info["databaseName"]
+    write_mode = metasave_info["mode"]
+    ms_list = metasave_info["measurementsName"]
     
-    def set_meta(self):
-        domain = self.db.split("_", maxsplit=1)[0]
-        sub_domain = self.db.split("_", maxsplit=1)[1]
-        mongodb_c = wiz.WizApiMongoMeta()
-        print(self.ms_list)
-        if type(self.ms_list) == str:
-            mongodb_c.post_database_collection_document(self.mode, self.meta_data, domain, sub_domain)
-            print("SUCCESS")
-        else:
-            mongodb_c.post_database_collection_documents(self.mode, self.meta_data, domain, sub_domain)
-            print("SUCCESS")
+    domain = db_name.split("_", maxsplit=1)[0]
+    sub_domain = db_name.split("_", maxsplit=1)[1]
+    mongodb_c = wiz.WizApiMongoMeta()
+    
+    print(ms_list)
+    if type(ms_list) == str:
+        mongodb_c.post_database_collection_document(write_mode, meta_data, domain, sub_domain)
+        print("SUCCESS")
+    else:
+        mongodb_c.post_database_collection_documents(write_mode, meta_data, domain, sub_domain)
+        print("SUCCESS")
