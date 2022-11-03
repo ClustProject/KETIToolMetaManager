914259ba0c0686365f9ddc884b0318db3fac7e52
jianso <dothewillof@naver.com>
2022-11-03 12:45:56 +0900

    update codes

--- metaController/DBmetaContoller.py
+++ metaController/DBmetaContoller.py
@@ -18,24 +18,27 @@ class metaController():
             
     # JIAN ####################################################################################
 
-    def save_db_label_meta2(self, path, filename, database_info_param):
+    def saveDBMeta(self, path, filename, database_info_param):
         """
         DataBase의 컬럼별 Label 정보 & 기본 컬럼 정보를 담은 Meta 를 생성 및 저장 하는 함수
         
         현재는 db_meta_json 을 물리 파일에서 추출하고 있으나, 
         추후 파라미터를 통해 라벨 정보를 넘길 예정이다.
         """
-        from KETIToolMetaManager.metaDataManager.descriptor import WriteData
+        from KETIToolMetaManager.metaDataManager.descriptor import write_data
        
 
         domain = database_info_param["databaseName"].split("_", maxsplit=1)[0]
         sub_domain = database_info_param["databaseName"].split("_", maxsplit=1)[1]
 
+        """
         db_meta_json["table_name"] = "db_information"
         db_meta_json["domain"] = domain
         db_meta_json["subDomain"] = sub_domain
+
+        """
         
-        WriteData(database_info_param, db_meta_json).set_meta()
+        write_data(database_info_param, {}).set_meta()
 
 
     # JIAN end ####################################################################################
